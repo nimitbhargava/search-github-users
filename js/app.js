8867acc8c29be20e17a927084153ec453d7fe7d7
Nimit Bhargava <nimitbhargava@gmail.com>
2017-09-28 20:31:43 +0530

    Knockout VM

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-/**
- * Created by nimit on 28-09-2017.
- */
+var ViewModel = function () {
+    var self = this;
+
+    filterUsers = [];
+
+    self.userInput = ko.observable('');
+    console.log("Initialized");
+    ko.computed(function () {
+        var result = [];
+        var query = self.userInput().toLowerCase();
+        console.log(query);
+    }, this);
+};
+
+ko.applyBindings(new ViewModel());
+
+$('.sidebar-toggle').click(function(){
+    $('.sidebar').toggleClass('d-none d-sm-block');
+    var updatedToggleBtnText = $('.sidebar-toggle').text() === "Collapse" ? "Explore" : "Collapse";
+    $('.sidebar-toggle').text(updatedToggleBtnText);
+});
