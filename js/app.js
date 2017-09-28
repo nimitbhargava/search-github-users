11d0707cf9a5f2b578ef269f0e2a479d63fadef7
Nimit Bhargava <nimitbhargava@gmail.com>
2017-09-28 21:00:31 +0530

    When a user types user name, wait for 500 miliseconds to reflect on the value to change - and then send AJAX request to fetch list of users.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,32 @@
 var ViewModel = function () {
     var self = this;
 
-    filterUsers = [];
+    $userInfo = $("#user-info");
 
     self.userInput = ko.observable('');
+    self.userInput.extend({rateLimit: 500})
     console.log("Initialized");
-    ko.computed(function () {
+    self.filterUsers = ko.computed(function () {
         var result = [];
         var query = self.userInput().toLowerCase();
-        console.log(query);
+        if (query === '') return [];
+        //Go to GitHub API and fetch the user
+        var url = 'http://api.github.com/search/users?q=' + query;
+        $.ajax({
+            type: 'GET',
+            url: url
+        }).done(function (data) {
+            result = data.items;
+        }).fail(function () {
+            result = [];
+        });
+        return result;
     }, this);
 };
 
 ko.applyBindings(new ViewModel());
 
-$('.sidebar-toggle').click(function(){
+$('.sidebar-toggle').click(function () {
     $('.sidebar').toggleClass('d-none d-sm-block');
     var updatedToggleBtnText = $('.sidebar-toggle').text() === "Collapse" ? "Explore" : "Collapse";
     $('.sidebar-toggle').text(updatedToggleBtnText);
